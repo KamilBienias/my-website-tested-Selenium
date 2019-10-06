edb5ce858e9099340794e9e17a94278ce267e4ce
KamilBienias <49995107+KamilBienias@users.noreply.github.com>
2019-10-06 22:17:28 +0200

    Update WebDriverSingleton.java
    path updated

--- src/test/java/config/WebDriverSingleton.java
+++ src/test/java/config/WebDriverSingleton.java
@@ -11,20 +11,8 @@ public class WebDriverSingleton {
     public static WebDriver getInstance() {
         if (null == driver) {//if the browser is not open
 
-            //it is deleted because it downloaded chromedriver78, but I can't update from 77 to 78
-//            WebDriverManager.chromedriver().setup();
-
-            //that is for firefox browser
-//            WebDriverManager.firefoxdriver().setup();
-
-            //manual chromedriver loading
-//            System.setProperty("webdriver.chrome.driver", "C:\\Users\\dom\\IdeaProjects\\MyWebsiteTestedSelenium\\src\\test\\java\\drivers\\chromedriver76.exe");
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\dom\\IdeaProjects\\MyWebsiteTestedSelenium\\src\\test\\java\\drivers\\chromedriver77.exe");
-//            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver77.exe");
-//            System.setProperty("webdriver.chrome.driver", "C:\\Users\\dom\\IdeaProjects\\MyWebsiteTestedSelenium\\src\\test\\java\\drivers\\chromedriver78.exe");
-
-            //relative path also doesn't work
-//            System.setProperty("webdriver.chrome.driver", "chromedriver77.exe");
+//            System.setProperty("webdriver.chrome.driver", "C:\\Users\\dom\\IdeaProjects\\MyWebsiteTestedSelenium\\src\\test\\java\\drivers\\chromedriver77.exe");
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver77.exe");
 
             driver = new ChromeDriver();//opens browser
         }
